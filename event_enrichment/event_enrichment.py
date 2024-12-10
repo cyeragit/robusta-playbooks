ff9eb62d6d1d13b604dd7fc65e933b2f7391bf22
Daniel Adler <daniela@cyera.io>
2024-12-10 16:16:28 +0200

    update event type

--- event_enrichment/event_enrichment.py
+++ event_enrichment/event_enrichment.py
@@ -22,18 +22,35 @@ def get_cluster_name(event: Union[EventChangeEvent, JobChangeEvent, PodEvent]) -
     return None
 
 
-@action
-def enrich_with_cluster_name(event: PodChangeEvent):
-    labels: Dict[str, Any] = defaultdict(lambda: "<missing>")
-    cluster_name = get_cluster_name(event)
-    if cluster_name:
-        labels["cluster"] = cluster_name
-        event.add_enrichment(
-            [MarkdownBlock(template.safe_substitute(labels))],
-        )
+# @action
+# def enrich_with_cluster_name(event: PodChangeEvent):
+#     cluster_name = get_cluster_name(event)
+#     if cluster_name:
+#         labels: Dict[str, Any] = defaultdict(lambda: "<missing>")
+#         labels.update(relevant_event_obj.metadata.labels)
+#         labels.update(relevant_event_obj.metadata.annotations)
+#         if event.obj.regarding.kind == "CronJob":
+#             logger.info(
+#                 f"Enriching cronjob labels -> {relevant_event_obj.spec.jobTemplate.spec.template.metadata.labels}")
+#             labels.update(relevant_event_obj.spec.jobTemplate.spec.template.metadata.labels)
+#         labels["name"] = relevant_event_obj.metadata.name
+#         labels["namespace"] = relevant_event_obj.metadata.namespace
+#         template = Template(params.template)
+#
+#         cluster_name = get_cluster_name(event)
+#
+#         for sink in event.named_sinks:
+#             for finding in event.sink_findings[sink]:
+#                 finding.subject.labels.update(labels)
+#                 if cluster_name:
+#                     labels["cluster"] = cluster_name
+#
+#         event.add_enrichment(
+#             [MarkdownBlock(template.safe_substitute(labels))],
+#         )
 
 @action
-def event_pod_label_enricher(event: EventChangeEvent, params: PodLabelTemplate):
+def event_pod_label_enricher(event: Union[EventChangeEvent, PodChangeEvent], params: PodLabelTemplate):
     logger.info(
         f"Enriching event with pod labels -> {event.obj.regarding.kind} - {event.obj.regarding.name} - {event.obj.regarding.namespace}")
 
